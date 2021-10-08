953251fe7fa59d7b2563b378439d610868ce5159
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-08 16:43:59 +0700

    Made Your Day

--- app/src/main/java/com/nuryadincjr/kotlinsesion/ZMS.kt
+++ app/src/main/java/com/nuryadincjr/kotlinsesion/ZMS.kt
@@ -1,9 +1,20 @@
 package com.nuryadincjr.kotlinsesion
 
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello ${args[0]}")
 
-    val isUnit = println("ini adalah expression")
-    println(isUnit)
+    feedTheTuewlu()
 }
 
+fun feedTheTuewlu() {
+    val day = randomDay()
+    val food = "Cap Cay"
+    println("Hari ini adalah hari $day jadi Simba makan $food")
+}
+
+fun randomDay()  : String {
+    val week = listOf("senin", "selasa", "Rabu", "Kamis", "Jum'at", "sabtu", "Minggu")
+    return week[Random().nextInt(7)]
+}
