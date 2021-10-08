4de2f73e80f7c027d0940861a7cda30f4b70dbee
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-08 17:04:42 +0700

    Feed yout pet, implement funcition return with parameter

--- app/src/main/java/com/nuryadincjr/kotlinsesion/ZMS.kt
+++ app/src/main/java/com/nuryadincjr/kotlinsesion/ZMS.kt
@@ -10,11 +10,35 @@ fun main(args: Array<String>) {
 
 fun feedTheTuewlu() {
     val day = randomDay()
-    val food = "Cap Cay"
+    val food = simbaFood(day)
     println("Hari ini adalah hari $day jadi Simba makan $food")
 }
 
+fun simbaFood(day: String) : String {
+    return when(day) {
+        "Senin" -> "Ikan asin"
+        "Selasa" ->  "Sayur kol"
+        "Rabu" ->  "Nasi padang"
+        "Kamis" -> "Karo uyah"
+        "Jum'at" -> "Susu beruang"
+        "Minggu" -> "Nasi goreng"
+        else -> "Puasa"
+    }
+
+//    sebelum penyederhanaan
+//    return foodvar food = "Puasa"
+//    when(day) {
+//        "Senin" -> food = "Ikan asin"
+//        "Selasa" -> food = "Sayur kol"
+//        "Rabu" -> food = "Nasi padang"
+//        "Kamis" -> food = "Karo uyah"
+//        "Jum'at" -> food = "Susu beruang"
+//        "Minggu" -> food = "Nasi goreng"
+//    }
+//    return food
+}
+
 fun randomDay()  : String {
-    val week = listOf("senin", "selasa", "Rabu", "Kamis", "Jum'at", "sabtu", "Minggu")
+    val week = listOf("Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "sabtu", "Minggu")
     return week[Random().nextInt(7)]
 }
