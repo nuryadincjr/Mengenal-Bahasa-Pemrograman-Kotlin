d75d7b2eb8b39a5c00e8ce6cd3bc7f675ad1c465
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-11 16:12:09 +0700

    Data Classes, Implement data container

---
+++ app/src/main/java/com/nuryadincjr/kotlinsesion/kandang/Decoration.kt
@@ -0,0 +1,38 @@
+package com.nuryadincjr.kotlinsesion.kandang
+
+fun main() {
+    makeDecoration()
+}
+fun makeDecoration() {
+    val dec1 = Decoration("Batu-Bata")
+    println(dec1)
+
+    val dec2 = Decoration("Batu-Akik")
+    println(dec2)
+
+    val dec3 = Decoration("Batu-Akik")
+    println(dec3)
+
+    println(Decoration("Marmer").equals(Decoration("Marmer")))
+    println(dec2.equals(dec3))
+
+    val dec4 = dec3.copy()
+    println(dec4)
+
+    val dec5 = DecorationNew("Batu-Ginjal", "Jati", "Eurih")
+    println(dec5)
+
+    val(rocks, wood, grass) = dec5
+
+    println(rocks)
+    println(wood)
+    println(grass)
+}
+
+data class Decoration(val rocks: String) {
+
+}
+
+data class DecorationNew(val rocks: String, val wood: String, val grass: String) {
+
+}
