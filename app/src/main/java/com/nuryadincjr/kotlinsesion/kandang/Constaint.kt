d062bdac7277aea6d62c51cb31e309888da7ff3e
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-14 16:06:12 +0700

    Constant, implement konsep compiletime & runtime

---
+++ app/src/main/java/com/nuryadincjr/kotlinsesion/kandang/Constaint.kt
@@ -0,0 +1,19 @@
+package com.nuryadincjr.kotlinsesion.kandang
+
+fun main() {
+    println(nuwNumbers)
+    println(numbers)
+    println(result)
+}
+
+const val numbers = 3
+val nuwNumbers = 100
+
+fun funcutionCall() {
+//    const val numbers = 3
+    val nuwNumbers = 100
+    println(nuwNumbers)
+}
+
+val result = funcutionCall()
+
