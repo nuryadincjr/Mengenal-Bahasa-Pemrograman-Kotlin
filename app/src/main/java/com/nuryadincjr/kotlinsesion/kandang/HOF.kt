9c771fc6b8563cbd4ec0722e214e4fa1df1344fb
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-15 21:03:56 +0700

    Inline funcition

--- app/src/main/java/com/nuryadincjr/kotlinsesion/kandang/HOF.kt
+++ app/src/main/java/com/nuryadincjr/kotlinsesion/kandang/HOF.kt
@@ -2,6 +2,23 @@ package com.nuryadincjr.kotlinsesion.kandang
 
 fun main() {
     contohCicak()
+
+    val a = 2
+    println(someMethod(a, { println("Funcition lain")}))
+
+    nonInlined {
+        println("do something here")
+    }
+
+    inlined {
+        println("do something here")
+    }
+
+}
+
+inline fun someMethod(a: Int, func: () -> Unit): Int {
+    func()
+    return 2*a
 }
 
 data class Cicak(var name: String)
@@ -12,4 +29,17 @@ fun contohCicak() {
     with(cicak.name) {
         println(capitalize())
     }
+}
+
+
+fun nonInlined(block: () -> Unit) {
+    println("before")
+    block()
+    println("after")
+}
+
+inline fun inlined(block: () -> Unit) {
+    println("before")
+    block()
+    println("after")
 }
